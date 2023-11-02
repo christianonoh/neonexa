bc6e413ca8cd49271aec5092f2e07048b55eed99
christianonoh <onohchibyk@gmail.com>
2023-11-02 08:02:16 +0100

    Modify layout to style

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from 'next/font/google'
 import '/styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'NeoNexa',
@@ -12,10 +10,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-    </head>
-      <body className={inter.className}>{children}</body>
+        <link rel="preconnect" href="https://stijndv.com" />
+        <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      </head>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='bg-primary-black overflow-hidden'>
       <Navbar />
       <Hero />
       <About />
