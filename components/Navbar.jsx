ce2d1195c0d78f531e27adcb1217ddfa86f9ead8
christianonoh <onohchibyk@gmail.com>
2023-11-02 08:00:53 +0100

    Create Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,39 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '@/utils/motion';
+import Image from 'next/image';
+
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient_01" />
+
+    <div className={`${styles.innerWidth} mx-auto flex justify-between items-center`}>
+      <Image
+        src="/icons/search.svg"
+        alt="search"
+        width={24}
+        height={24}
+        className='object-contain'
+      />
+      <h2 className='text-2xl font-extrabold text-white'>
+        NEONEXA
+      </h2>
+      <Image
+        src="/icons/menu.svg"
+        alt="menu"
+        width={24}
+        height={24}
+        className='object-contain'
+      />
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
