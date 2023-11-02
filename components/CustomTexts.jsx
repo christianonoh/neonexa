90cc11cfddfa19a628b4fc49a562adcf0cb436f6
christianonoh <onohchibyk@gmail.com>
2023-11-02 10:54:28 +0100

    Add TypingText effect component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,23 @@
 'use client';
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import { textContainer, textVariant2 } from "@/utils/motion";
+import { motion } from "framer-motion";
+
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-secondary-white text-sm ${textStyles}`}
+  >
+    {Array.from(title).map((letter, index) => (
+      <motion.span
+        key={index}
+        variants={textVariant2}
+      >
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    )
+    )}
+  </motion.p>
 );
 
 export const TitleText = () => (
