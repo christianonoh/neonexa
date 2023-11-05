5f38108200c165e1944b8f2b58dd9b24c8e8e979
christianonoh <onohchibyk@gmail.com>
2023-11-05 05:33:07 +0100

    Create Feedback section

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,60 @@
 'use client';
+import { motion } from "framer-motion";
+import styles from "@/styles";
+import { fadeIn, staggerContainer, zoomIn } from "@/utils/motion";
 
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} flex lg:flex-row flex-col gap-6 mx-auto`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="sm:p-8 p-4 flex flex-col flex-[0.5] gradient-05
+         rounded-3xl max-w-[370px] text-white justify-end border
+          border-[#6a6a6a] relative"
+      >
+        <div className="feedback_gradient" />
+        <div>
+          <h4 className=" font-bold text-3xl">
+              Samantha
+            </h4>
+            <p className="text-lg font-normal mt-1">
+              Founder NeoNexa
+            </p>
+        </div>
+        <p className="text-2xl leading-10 mt-6">
+        “With the development of today's technology, metaverse is very
+         useful for today's work, or can be called web 3.0. by using metaverse
+          you can use it as anything”
+        </p>
+      </motion.div>
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className={`relative flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/images/planet-09.png"
+          alt="planet 09"
+          className="w-full lg:h-[610px] h-auto min-h-[210px] object-cover rounded-3xl"
+        />
+
+        <motion.div
+          variants={zoomIn(0.3, 1)}
+          className="absolute -left-[10%] top-[7%] lg:block hidden z-10"
+        >
+          <img
+            src="/images/stamp.png"
+            alt="stamp"
+            className="w-[155px] h-auto"
+          />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
