70f738e085f83c44020e6a7200847610a88ac795
christianonoh <onohchibyk@gmail.com>
2023-11-05 15:50:09 +0100

    Remove redundant lines

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import { Footer, Navbar } from '@/components'
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '@/sections'
-import Image from 'next/image'
 
 export default function Home() {
   return (
